f1321dc91d48524ed1b46af30105a858a409a66b
Roman Simone <83060406+Roman-Simone@users.noreply.github.com>
2024-09-20 10:47:10 +0200

    update save frame with legend and camera

--- simo/saveFrame.py
+++ simo/saveFrame.py
@@ -4,6 +4,7 @@ import os
 import re
 import cv2
 import json
+import numpy as np
 from utils import *
 from cameraInfo import *
 
@@ -60,7 +61,19 @@ camera_coordinates_dict = {
     8: [-22.0, -10.0, 6.3],
     12: [-22.0, 10.0, 6.9],
     13: [22.0, 0.0, 7.0]
+}
 
+camera_coordinates_visual = {
+    1: [790, 30],
+    2: [410, 30],
+    3: [790, 70],
+    4: [35, 35],
+    5: [790, 470],
+    6: [410, 470],
+    7: [40, 250],
+    8: [40, 470],
+    12: [40, 80],
+    13: [790, 250]
 }
 
 
@@ -95,7 +108,7 @@ def unifyImages(img1, img2, rightCameraFlag):
     # Get dimensions of the resized img2
     rows, cols, channels = img2_resized.shape
 
-    # Define the position where the logo will be placed
+    # Define the position where the court will be placed
     x_margin = 50  # Margin from the edge
     y_offset = 50  # Vertical offset from the top
 
@@ -123,13 +136,97 @@ def unifyImages(img1, img2, rightCameraFlag):
     dst = cv2.add(img1_bg, img2_fg)
     img1[y_offset:y_offset+rows, x_offset:x_offset+cols] = dst
 
+    _, width, _ = img2_resized.shape
+
+    img1 = add_legend(img1, width, rightCameraFlag)
+
     return img1
 
 
-def edit_image(image):
+def add_legend(image, width_court, rightCameraFlag):
+    # Dimensioni del testo e colore
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.7
+    color_text = (255, 255, 255)  # Bianco
+    thickness_text = 2
+
+    # Distanza verticale tra le voci della leggenda
+    first_y_offset = 20
+    y_offset = 40
+
+    if rightCameraFlag:
+        _, image_width, _ = image.shape
+        x_offset = (image_width ) - (width_court) - 400   # Margine destro della leggenda
+    else:
+        x_offset = width_court + 80   # Margine sinistro della leggenda
+
+    # Legenda per i cerchi e il triangolo
+    legend_items = [
+        {"text": "Corner to select", "color": (0, 170, 255)},  # giallo
+        {"text": "Corner already selected", "color": (0, 255, 0)},  # verde
+        {"text": "Corner skipped", "color": (255, 0, 0)},  # blu
+        {"text": "Position of camera", "color": (139, 139, 0)}  # colore triangolo
+    ]
+
+    # Itera attraverso gli elementi della leggenda
+    for i, item in enumerate(legend_items):
+        # Calcola la posizione per il testo e i simboli
+        y_position = (i + 1) * y_offset + first_y_offset
+
+        # Disegna il simbolo (cerchio o triangolo)
+        if "Triangolo" in item["text"]:
+            # Vertici del triangolo
+            triangle_side = 13  # Definisci un triangolo più piccolo per la leggenda
+            triangle_points = np.array([
+                [x_offset, y_position],  # Vertice superiore
+                [x_offset - triangle_side, y_position + triangle_side],  # Basso a sinistra
+                [x_offset + triangle_side, y_position + triangle_side]  # Basso a destra
+            ], np.int32)
+            triangle_points = triangle_points.reshape((-1, 1, 2))
+            cv2.fillPoly(image, [triangle_points], color=item["color"])
+        else:
+            # Disegna il cerchio colorato
+            cv2.circle(image, (x_offset, y_position), 13, item["color"], -1)
+
+        # Aggiungi la descrizione accanto al simbolo
+        cv2.putText(image, item["text"], (x_offset + 30, y_position + 5), font, font_scale, color_text, thickness_text)
+
+    return image
+
+
+def edit_image(image, camera_number= 1):
 
     # Thickness of the point
     point_thickness = 20
+    
+    width, height, _ = image.shape
+
+    for camera in camera_coordinates_visual:
+        if camera == camera_number:
+            # camera_coordinates = camera_coordinates_dict[camera]
+            # print(camera_coordinates)
+            # camera_x = int((width / 2) + (camera_coordinates[0] * 20))
+            # camera_y = int((height / 2) - (camera_coordinates[1]) * 10)
+            camera_x = camera_coordinates_visual[camera][0]
+            camera_y = camera_coordinates_visual[camera][1]
+
+            # cv2.circle(image, (int(camera_x), int(camera_y)), point_thickness, (255, 0, 0), -1)
+            # Definisci il lato del triangolo
+            triangle_side = 20  # Puoi regolare questo valore per dimensioni diverse
+
+            # Calcola i vertici del triangolo relativi a camera_x e camera_y
+            trianglePoints = np.array([
+                [camera_x, camera_y - triangle_side],  # Vertice superiore
+                [camera_x - triangle_side, camera_y + triangle_side],  # Vertice in basso a sinistra
+                [camera_x + triangle_side, camera_y + triangle_side]  # Vertice in basso a destra
+            ], np.int32)
+
+            # Ridimensiona i punti per il formato richiesto da OpenCV
+            trianglePoints = trianglePoints.reshape((-1, 1, 2))
+
+            # Disegna il triangolo sull'immagine
+            cv2.fillPoly(image, [trianglePoints], color=(139, 139, 0)) #0,139,139
+            break
 
     for point in points:
         point_x = point[0]
@@ -150,21 +247,21 @@ def edit_image(image):
     return image
 
 
-def takePoints(imageUndistorted, courtImg, rightCameraFlag):
+def takePoints(imageUndistorted, courtImg, camera_number, rightCameraFlag):
     global clicked_point, points
+
     print("Select the corners of the court")
-    window_name = 'Select Points'
+    window_name = f"Select Points camera {camera_number}"
     cv2.namedWindow(window_name)
     cv2.setMouseCallback(window_name, on_mouse)
 
     img_copy = imageUndistorted.copy()
-    image_points = []
 
     for point in points:
         if points[point] == 0:
             while True:
                 # img_with_points = edit_image(img_copy.copy())
-                courtImgEdited = edit_image(courtImg)
+                courtImgEdited = edit_image(courtImg, camera_number)
                 image = unifyImages(img_copy, courtImgEdited, rightCameraFlag)
 
                 cv2.imshow(window_name, image)
@@ -271,14 +368,15 @@ def saveFrames():
 
             # undistorted_frame = unifyImages(undistorted_frame, courtImg_with_points, rightCameraFlag)
 
-            cv2.imshow('image', undistorted_frame)
+            cv2.imshow(f"Camera {camera_number}", undistorted_frame)
             key = cv2.waitKey(0)
             if key == ord('s'):
+
                 frame_filename = os.path.join(path_frames, f"cam_{camera_number}.png")
                 cv2.imwrite(frame_filename, undistorted_frame)
                 cv2.destroyAllWindows()
 
-                world_image_coordinates = takePoints(undistorted_frame_copy, courtImg, rightCameraFlag)
+                world_image_coordinates = takePoints(undistorted_frame_copy, courtImg, camera_number, rightCameraFlag)
 
                 print(world_image_coordinates)
                 # Save worldPoints and imagePoints to the global dictionary
