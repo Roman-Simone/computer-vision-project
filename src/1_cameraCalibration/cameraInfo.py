540a083f748d932e67e5cdf99fad5b4d8fe90d9e
Stefano Bonetto <stefanoobonetto@gmail.com>
2024-11-13 09:12:45 +0100

    new_changes

--- src/1_cameraCalibration/cameraInfo.py
+++ src/1_cameraCalibration/cameraInfo.py
@@ -1,29 +1,69 @@
 class CameraInfo:
+    """
+    A class to represent the information and calibration data of a camera.
+
+    Attributes
+    ----------
+    camera_number : int
+        The identifier number of the camera.
+    chessboard_size : tuple or None
+        The size of the chessboard used for calibration (rows, columns).
+    objpoints : list
+        The list of 3D points in real world space.
+    imgpoints : list
+        The list of 2D points in image plane.
+    mtx : numpy.ndarray or None
+        The camera matrix.
+    newcameramtx : numpy.ndarray or None
+        The new camera matrix after undistortion.
+    dist : numpy.ndarray or None
+        The distortion coefficients.
+    roi : tuple or None
+        The region of interest.
+    extrinsic_matrix : numpy.ndarray or None
+        The extrinsic parameters.
+
+    Methods
+    -------
+    __str__():
+        Returns a string representation of the CameraInfo object.
+    """
     def __init__(self, camera_number):
         self.camera_number = camera_number 
-        self.chessboard_size = None     # Size of the chessboard used for calibration
-        self.objpoints = []             # 3D points in real world space
-        self.imgpoints = []             # 2D points in image plane
-        self.mtx = None                 # Camera matrix
-        self.newcameramtx = None        # New camera matrix after undistortion
-        self.dist = None                # Distortion coefficients
-        self.roi = None                 # Region of interest
-        self.extrinsic_matrix = None    # Extrinsic parameters
+        self.chessboard_size = None     
+        self.objpoints = []             
+        self.imgpoints = []             
+        self.mtx = None                 
+        self.newcameramtx = None        
+        self.dist = None
+        self.roi = None                 
+        self.extrinsic_matrix = None    
 
     def __str__(self):
-        """Return a string representation of the CameraInfo object
-        """
         return f"Camera number: {self.camera_number}"
 
 
 class HomographyInfo:
+    """
+    A class to represent the homography information between two cameras.
+    Attributes
+    ----------
+    camera_number_1 : int
+        The number of the first camera.
+    camera_number_2 : int
+        The number of the second camera.
+    homography : numpy.ndarray or None
+        The homography matrix between the two cameras. Initialized to None.
+    Methods
+    -------
+    __str__():
+        Returns a string representation of the HomographyInfo object.
+    """
+    
     def __init__(self, camera_number_1, camera_number_2):
-        # Initialize the HomographyInfo object with the given camera numbers
-        self.camera_number_1 = camera_number_1
+        self.camerqa_number_1 = camera_number_1
         self.camera_number_2 = camera_number_2
-        self.homography = None      
+        self.homography = None
     
     def __str__(self):
-        """Return a string representation of the HomographyInfo object
-        """
         return f"Camera 1: {self.camera_number_1}, Camera 2: {self.camera_number_2}"
